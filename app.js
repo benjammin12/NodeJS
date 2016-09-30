4d43a1bd5543328116bc8a6fc22d94b83e435d96
benjammin12 <benxerri@gmail.com>
2016-09-30 00:08:20 -0400

    Prototypes with node/javascript

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-//grab firstNode.js file in the same location and run it
-var test = require('./firstNode.js');
+//grab figletText.js file in the same location and run it
+var test = require('./figletText.js');
+
+function Checking(firstName,lastName, amount){  //creating objects in javascript/nodejs
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.amount = amount;
+}
+
+Checking.prototype.greet = function () { //can create a method that works for all prototype objects of "Checking"
+  console.log(this.firstName + " " + this.lastName);
+};
+
+var mike = new Checking("Mike", "Smith", 500); //create a checking object of mike, similar to java
+
+
+console.log("Hello " +  mike.firstName + " you currently have " + mike.amount);
+
+mike.greet(); 
